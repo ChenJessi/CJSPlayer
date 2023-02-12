085ffa76e955b4dd74f7be64487cdbb7e582ebd8
ChenJessi <188669@163.com>
2023-02-12 15:52:57 +0800

    Delete some obsolete codes

--- app/src/main/java/com/chen/cjsplayer/listener/CyOnValumeDBListener.java
+++
@@ -1,9 +0,0 @@
-package com.chen.cjsplayer.listener;
-
-/**
- * @author Created by CHEN on 2019/3/13
- * @email 188669@163.com
- */
-public interface CyOnValumeDBListener {
-    void onDbValue(int db);
-}
