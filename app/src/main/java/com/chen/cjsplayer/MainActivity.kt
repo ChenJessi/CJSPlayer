4a1eff0fc447ae287ceadd1c8b652c5c21c5a3f0
ChenJessi <188669@163.com>
2023-02-12 15:39:22 +0800

    gradle config update

--- app/src/main/java/com/chen/cjsplayer/MainActivity.kt
+++ app/src/main/java/com/chen/cjsplayer/MainActivity.kt
@@ -2,10 +2,12 @@ package com.chen.cjsplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
